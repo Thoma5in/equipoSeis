d2e82203f99b8fa4829def5b5727cc07eb3a6aa0
luiseurban <165191210+luiseurban@users.noreply.github.com>
2025-12-03 11:53:12 -0500

    Actualizar dependencias para los test unitarios
    Se actualizan las dependencias necesarias para los tests. Tambien se crean los tests para el HomeViewModel.

--- app/src/test/java/com/example/inventoryapp/viewmodel/HomeViewModelTest.kt
+++ app/src/test/java/com/example/inventoryapp/viewmodel/HomeViewModelTest.kt
@@ -1,12 +1,46 @@
 package com.example.inventoryapp.viewmodel
 
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.inventoryapp.FakeListenerRegistration
+import com.example.inventoryapp.model.Producto
+import com.example.inventoryapp.repository.FirestoreInventoryRepository
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.Assert.*
+import org.mockito.kotlin.*
+
+
 
 class HomeViewModelTest {
-    @BeforeEach
-    fun setUp() {
-        TODO("Not yet implemented")
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private val repository: FirestoreInventoryRepository = mock()
+
+    @Test
+    fun `init loads products and updates LiveData`() {
+        var callback: ((List<Producto>) -> Unit)? = null
+
+        whenever(repository.getAllProductos(any())).thenAnswer {
+            callback = it.arguments[0] as (List<Producto>) -> Unit
+            FakeListenerRegistration()
+        }
+
+        val vm = HomeViewModel(repository)
+
+        val listaFake = listOf(Producto(1, "p1", 1000.0, 2))
+        callback!!.invoke(listaFake)
+
+        assertFalse(vm.isLoading.value!!)
+        assertEquals(1, vm.allProducts.value!!.size)
     }
 
+    @Test
+    fun `deleteProductById calls repository delete`() = runTest {
+        val vm = HomeViewModel(repository)
+        vm.deleteProductById(10)
+        verify(repository).delete(10)
+    }
 }
