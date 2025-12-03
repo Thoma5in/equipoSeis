c6b8d50a9fb37c7b3d2cd6ed5bb41f9cc55f5fe8
luiseurban <165191210+luiseurban@users.noreply.github.com>
2025-12-02 23:20:18 -0500

    Crear los Test de cada View Model

---
+++ app/src/test/java/com/example/inventoryapp/viewmodel/HomeViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.example.inventoryapp.viewmodel
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+
+class HomeViewModelTest {
+    @BeforeEach
+    fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/test/java/com/example/inventoryapp/viewmodel/LoginViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.example.inventoryapp.viewmodel
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+
+class LoginViewModelTest {
+    @BeforeEach
+    fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+}
