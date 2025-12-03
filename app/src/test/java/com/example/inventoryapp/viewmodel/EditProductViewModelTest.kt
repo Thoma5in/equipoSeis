8b1f3cc4a374cb80ed17b59692b5c33e8ee434d4
luiseurban <165191210+luiseurban@users.noreply.github.com>
2025-12-03 15:59:28 -0500

    Crear Test para los ViewModel deEditProduct y Login

--- app/src/test/java/com/example/inventoryapp/viewmodel/EditProductViewModelTest.kt
+++ app/src/test/java/com/example/inventoryapp/viewmodel/EditProductViewModelTest.kt
@@ -1,12 +1,68 @@
 package com.example.inventoryapp.viewmodel
 
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.inventoryapp.model.Producto
+import com.example.inventoryapp.repository.FirestoreInventoryRepository
+import com.google.firebase.firestore.ListenerRegistration
+import getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.kotlin.*
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class EditProductViewModelTest {
-    @BeforeEach
-    fun setUp() {
-        TODO("Not yet implemented")
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val repository: FirestoreInventoryRepository = mock()
+    private lateinit var viewModel: EditProductViewModel
+
+    private val listenerRegistration: ListenerRegistration = mock()
+    private val testDispatcher = StandardTestDispatcher()
+
+    @Before
+    fun setup() {
+        viewModel = EditProductViewModel(repository)
     }
 
+    @Test
+    fun `getProduct retorna el producto esperado`() = runTest {
+
+        val producto = Producto(1, "Café", 10.00)
+
+        whenever(repository.getProductById(eq(1), any())).thenAnswer {
+            val callback = it.arguments[1] as (Producto?) -> Unit
+            callback(producto)
+            listenerRegistration
+        }
+
+        val result = viewModel.getProduct(1).getOrAwaitValue()
+
+        assertEquals(producto, result)
+    }
+
+    @Test
+    fun `updateProduct llama repository update correctamente`() = runTest(testDispatcher) {
+        // Arrange
+        val producto = Producto(
+            codigo = 1,
+            nombre = "Mouse Gamer",
+            precio = 150000.0,
+            cantidad = 5
+        )
+
+        // Act
+        viewModel.updateProduct(producto)
+        advanceUntilIdle()
+
+        // Assert
+        verify(repository).update(producto)
+    }
 }
